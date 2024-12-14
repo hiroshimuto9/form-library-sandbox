1f3c1d43181bbc1fa7139dc8839909c124b65210
Hiroshi Muto <mhi6o461o@gmail.com>
2024-12-14 20:22:52 +0900

    registerの検証

--- src/app/rhf/_components/Form.tsx
+++ src/app/rhf/_components/Form.tsx
@@ -1,19 +1,66 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+
+type FormValues = {
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  category: string;
+  about: string;
+  age: number;
+};
+
 export function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    defaultValues: {
+      name: {
+        firstName: "bill",
+        lastName: "luo",
+      },
+      category: "B",
+      about: "This is a test",
+      age: 0,
+    },
+  });
+  console.log("errors", errors);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <form className="space-y-4">
+    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
       <input
+        {...register("name.firstName", {
+          required: "firstName is required",
+          maxLength: 5,
+        })}
         placeholder="First name"
         className="w-full p-3 text-sm border border-black rounded-md bg-transparent"
       />
-      <select className="w-full p-3 text-sm border border-black rounded-md bg-transparent">
+      <select
+        {...register("category")}
+        className="w-full p-3 text-sm border border-black rounded-md bg-transparent"
+      >
         <option value="">Select...</option>
         <option value="A">Option A</option>
         <option value="B">Option B</option>
       </select>
       <textarea
+        {...register("about")}
         placeholder="About you"
+        className="w-full p-3 text-sm border border-black rounded-md bg-transparent"
+      />
+      <input
+        {...register("age", { valueAsNumber: true })}
+        type="number"
+        placeholder="10"
         className="w-full p-3 text-sm border border-black rounded-md bg-transparent"
       />
       <input
--- src/app/rhf/_components/Header.tsx
+++ src/app/rhf/_components/Header.tsx
@@ -3,13 +3,13 @@
 import { useRef } from "react";
 
 export default function Header() {
-  const renderCount = useRef(0);
-  renderCount.current++;
+  // const renderCount = useRef(0);
+  // renderCount.current++;
 
   return (
     <div className="mb-10">
       <span className="fixed top-5 right-8 z-50 font-normal py-2 px-4 rounded-md">
-        Render Count: {renderCount.current}
+        {/* Render Count: {renderCount.current} */}
       </span>
       <h1 className="mt-20 text-2xl flex items-center">React Hook Form</h1>
       <p className="text-sm leading-snug mb-0">
