d3431ee8c0c82e5ae1e486eefc16a4ba930c5de9
Hiroshi Muto <mhi6o461o@gmail.com>
2024-12-15 12:20:07 +0900

    ラジオボタンとチェックボックスを追加

--- src/app/rhf/_components/Form.tsx
+++ src/app/rhf/_components/Form.tsx
@@ -13,10 +13,13 @@ type FormValues = {
   about: string;
   age: number;
   likes: { value: string; label: string }[];
+  gender: string;
+  week: string[];
 };
 
 export function Form() {
   const {
+    watch,
     control,
     register,
     handleSubmit,
@@ -31,9 +34,11 @@ export function Form() {
       about: "This is a test",
       age: 0,
       likes: [{ value: "chocolate", label: "Chocolate" }],
+      gender: "1",
+      week: ["1"],
     },
   });
-  console.log("errors", errors);
+  const formValues = watch();
 
   const onSubmit = (data: any) => {
     console.log(data);
@@ -69,6 +74,63 @@ export function Form() {
         placeholder="10"
         className="w-full p-3 text-sm border border-black rounded-md bg-transparent"
       />
+      <div>
+        <label>
+          <input
+            type="radio"
+            value="1"
+            {...register("gender", { required: "Gender is required" })}
+          />
+          男性
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="2"
+            {...register("gender", { required: "Gender is required" })}
+          />
+          女性
+        </label>
+        {errors.gender && <p>{errors.gender.message}</p>}
+      </div>
+      <div>
+        <label>
+          <input
+            value="1"
+            type="checkbox"
+            {...register("week")}
+            className="mr-2"
+          />
+          春
+        </label>
+        <label>
+          <input
+            value="2"
+            type="checkbox"
+            {...register("week")}
+            className="mr-2"
+          />
+          夏
+        </label>
+        <label>
+          <input
+            value="3"
+            type="checkbox"
+            {...register("week")}
+            className="mr-2"
+          />
+          秋
+        </label>
+        <label>
+          <input
+            value="4"
+            type="checkbox"
+            {...register("week")}
+            className="mr-2"
+          />
+          冬
+        </label>
+      </div>
       <ClientOnly>
         <Controller
           name="likes"
@@ -89,6 +151,9 @@ export function Form() {
         type="submit"
         className="w-full p-5 mt-5 text-sm font-semibold tracking-widest uppercase bg-[#ec5990] rounded-md cursor-pointer hover:bg-[#bf1650] transition-colors duration-300"
       />
+      <div>
+        <pre>{JSON.stringify(formValues, null, 2)}</pre>
+      </div>
     </form>
   );
 }
